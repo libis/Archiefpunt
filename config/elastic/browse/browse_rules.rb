4f7aa5e8bfc15c31a35935bb05ee7ce13b643523
Mehmet Celik <mehmet@celik.be>
2022-06-30 10:53:54 +0200

    beheerder, samensteller correct id, updated browse rules, elastic 7 day mapping

--- config/elastic/browse/browse_rules.rb
+++ config/elastic/browse/browse_rules.rb
@@ -4,7 +4,11 @@ RULES = {
       '@' => lambda do |d, query|
         result = []
         out = DataCollector::Output.new
-        rules_ng.run(RULES['browse'], d, out, query)
+        unless filter(query, '$..range').empty?
+          rules_ng.run(RULES['browse2'], d, out, query)
+        else
+          rules_ng.run(RULES['browse'], d, out, query)
+        end
         result << out[:data]
         out.clear
 
@@ -12,6 +16,13 @@ RULES = {
         result.uniq! unless result.empty? || result.nil?
 
         result.compact
+      end
+    }
+  },
+  'browse2' => {
+    'data' => {
+      '@.hits.hits[*]._source.archief' => lambda do |d, query|
+        {id: d['id'], titel: d['auto'].map{|a| a['archief_titel'].map{|m| m['waarde']}}.flatten, laatste_wijziging: d['generated_date']}
       end
     }
   },
@@ -28,7 +39,15 @@ RULES = {
         #findex = index.split('.').join('..')
         #candidates = d.map{|m| m[index]||filter(m, "$..#{findex}")}
         findex = index.split('.')
-        candidates = d.map{|m| m[index]&.grep(/#{browse}/i) || filter(m, "$..#{findex[0]}[?(@['#{findex[1]}'] =~ /#{browse}/i)]")}
+
+        #candidates = d.map{|m| m[index]&.grep(/#{browse}/i) || filter(m, "$..#{findex[0]}[?(@['#{findex[1]}'] =~ /#{browse}/i)]")}
+        candidates = d.map do |m|
+          i = m[index]
+          i = [i] unless i.is_a?(Array)
+
+          i&.grep(/#{browse}/i) | filter(m, "$..#{findex.join('..')}")&.grep(/#{browse}/i)
+        end
+
         candidates = [candidates] unless candidates.is_a?(Array)
 
         #candidates.flatten.grep(/#{browse}/i)
